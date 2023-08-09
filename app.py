d5e97a7db8562b7e1f4eba46aa5da55e2b850655
zaynabzahra <zaynabzahra@gmail.com>
2023-08-09 14:44:33 +0100

    updating

--- app.py
+++ app.py
@@ -4,69 +4,41 @@ from dispel4py.new.simple_process import process as simple_process
 from dispel4py.new.multi_process import process as multi_process
 from dispel4py.new.dynamic_redis import process as dyn_process
 import codecs
+#import shutil
 import cloudpickle as pickle 
 from flask import Flask, request
-from io import StringIO
-import sys
 from easydict import EasyDict as edict
+from io import StringIO 
 import re
+import os
+import subprocess 
+import sys
 
-app = Flask(__name__)
+def install(package):
+    subprocess.call(['pip', 'install', package])
 
-@app.route('/run', methods=['GET', 'POST'])
-def run_workflow():
+def deserialize_directory(data,path):
 
-    #todo check if request is post and error handle each param 
+    if data == None: 
+        return None 
 
-    data = request.get_json()
+    for item, item_data in data.items():
 
-    workflow_id = data["workflowId"]
-    workflow = data["graph"]
-    inputCode = data["inputCode"]
-    process = data["process"]
-    args = data["args"]
+        item_path = os.path.join(path,item)
 
-    if workflow: #checking if user specified graph in registry
-        workflow_code = workflow["workflowCode"]
-    else:
-        workflow_code = data["workflowCode"] #direct code 
+        if item_data["type"] == "file":
 
-    unpickled_workflow_code  = pickle.loads(codecs.decode(workflow_code.encode(), "base64"))
-    unpickled_input_code  = pickle.loads(codecs.decode(inputCode.encode(), "base64"))
-    unpickled_args_code: dict = pickle.loads(codecs.decode(args.encode(), "base64"))
+            with open(item_path,"w") as f:
+                  file_content = item_data["content"]
+                  f.write(file_content)
 
-    graph: WorkflowGraph = unpickled_workflow_code   #Code execution 
-    nodes = graph.getContainedObjects() #nodes in graph 
-    producer = get_first(nodes) # Get first PE in graph
+        elif item_data["type"] == "directory":
 
-    if unpickled_args_code is not None :
-        args_dict = edict(unpickled_args_code)
-    else:
-        args_dict = None 
+            os.makedirs(item_path,exist_ok=True)
+            deserialize_directory(item_data["contents"], item_path)
 
-    buffer = StringIO()
-    sys.stdout = buffer 
-
-    if process == 1: 
-        print("Executing workflow with simple process")
-        simple_process(graph, {producer: unpickled_input_code},args_dict)
-        print_output = buffer.getvalue()
-    elif process == 2:
-        print("Executing workflow with multi process")
-        multi_process(graph, {producer: unpickled_input_code},args_dict)
-        print_output = buffer.getvalue()
-       
-    elif process == 3:
-        print("Executing workflow with dynamic process")
-        dyn_process(graph, {producer: unpickled_input_code},args_dict) #args as dictionary
-        print_output = buffer.getvalue()
-    else: 
-        return {"result": "N\A"}, 500
-    
-    sys.stdout = sys.__stdout__
-
-    return {"result": print_output}, 201
-    
+def deserialize(data):
+    return pickle.loads(codecs.decode(data.encode(), "base64"))
 
 def get_first(nodes:list):
 
@@ -80,67 +52,71 @@ def get_first(nodes:list):
         
     return id_dict[min_id]  
 
+app = Flask(__name__)
+@app.route('/run', methods=['GET', 'POST'])
+def run_workflow():
 
+    data = request.get_json()
 
+    workflow_id = data["workflowId"]
+    workflow = data["graph"]
+    inputCode = data["inputCode"]
+    process = data["process"]
+    args = data["args"]
+    resources = data["resources"]
+    imports = data["imports"]
 
+    import_list = list(filter(None, imports.split(',')))
+    
+    print("import list :", import_list)
 
+    for _import in import_list: #handle imports 
+        install(_import)
 
+    if workflow: #checking if user specified graph in registry
+        workflow_code = workflow["workflowCode"]
+    else:
+        workflow_code = data["workflowCode"] #direct code 
 
+    unpickled_workflow_code  = deserialize(workflow_code)
+    unpickled_input_code  = deserialize(inputCode)
+    unpickled_args_code: dict = deserialize(args)
+    unpickled_resources_code = deserialize(resources)
 
+    deserialize_directory(unpickled_resources_code,"resources/")  #make resources directory
 
+    graph: WorkflowGraph = unpickled_workflow_code #Code execution 
+    nodes = graph.getContainedObjects() #Nodes in graph 
+    producer = get_first(nodes) #Get first PE in graph
 
+    if unpickled_args_code is not None :
+        args_dict = edict(unpickled_args_code)
+    else:
+        args_dict = None 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#REMOVE
-#list = []
-#from dispel4py.dispel4py.new.simple_process import process as simple_process
-##
-
-#deserialize pes 
-
-#deserialise graph
-#graph = 
-
-#simple_process(graph, {first_pe})
-
-
-#TEST - remove
-#---------------
-
-       #print("\nUnpickled code\n")
-        #code = response["peCode"]
-        #unpickled = pickle.loads(codecs.decode(code.encode(), "base64"))
-        #list.append(unpickled)
-
-
-             #print("\nUnpickled code\n")
-
-        #code = response["workflowCode"]
-
-        #unpickled = pickle.loads(codecs.decode(code.encode(), "base64"))
-
-        #list.append(unpickled)
-
-        #if(len(list) > 2): 
-
-        #    pe1 = list[0]
-        #    pe2 = list[1]
-        #    graph: WorkflowGraph = list[2]
-        #    simple_process(graph, {pe1: 1})
-
+    buffer = StringIO() #redirect terminal output to return to client
+    sys.stdout = buffer 
+   
+    if process == 1: #Check type of mapping
+        print("Executing workflow with simple process")
+        simple_process(graph, {producer: unpickled_input_code},args_dict)
+        print_output = buffer.getvalue()
+    elif process == 2:
+        print("Executing workflow with multi process")
+        multi_process(graph, {producer: unpickled_input_code},args_dict)
+        print_output = buffer.getvalue()
+       
+    elif process == 3:
+        print("Executing workflow with dynamic process")
+        dyn_process(graph, {'producer': unpickled_input_code},args_dict) 
+        print_output = buffer.getvalue()
+    else: 
+        return {"result": "N\A"}, 500
+    
+    sys.stdout = sys.__stdout__
+    
+    #clear resources directory
+    #shutil.rmtree('resources/')
+        
+    return {"result": print_output}, 201
+    
